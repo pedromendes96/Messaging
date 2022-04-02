06ba935a9376a388efcff37a307c60d5c389dc29
Pedro Mendes <pedro.trabalho.uma@gmail.com>
2022-04-02 16:13:54 +0100

    Test

--- src/main/java/com/pedro/learn/messaging/MessagingApplication.java
+++ src/main/java/com/pedro/learn/messaging/MessagingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessagingApplication {
 
     public static void main(String[] args) {
+        System.out.println("HI");
         SpringApplication.run(MessagingApplication.class, args);
     }
 
