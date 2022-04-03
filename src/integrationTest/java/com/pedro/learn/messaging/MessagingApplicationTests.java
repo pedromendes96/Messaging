e3c6c1413d615c438ce701da2140b2c86603ffe8
Pedro Mendes <pedro.trabalho.uma@gmail.com>
2022-04-03 02:08:52 +0100

    Separate integration tests from unit tests

--- src/integrationTest/java/com/pedro/learn/messaging/MessagingApplicationTests.java
+++ src/integrationTest/java/com/pedro/learn/messaging/MessagingApplicationTests.java
@@ -1,14 +1,13 @@
 package com.pedro.learn.messaging;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class MessagingApplicationTests {
 
     @Test
     void contextLoads() {
-        assertEquals(1, 1);
     }
 
 }
--- src/main/java/com/pedro/learn/messaging/MessagingApplication.java
+++ src/main/java/com/pedro/learn/messaging/MessagingApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessagingApplication {
 
     public static void main(String[] args) {
-        System.out.println("HI");
         SpringApplication.run(MessagingApplication.class, args);
     }
 
